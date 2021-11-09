1a4cadc38a8e0f6e6622fc19d54dd4966019ba64
Daniel <danielpas317@gmail.com>
2021-11-09 10:37:26 +0100

    Initial Commit

--- game.js
+++ game.js
@@ -1,24 +1,25 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "Teal";
-ctx.fillRect(0, 0, 1280, 720);
+ctx.fillRect(0, 0, 640, 480);
 
 var bee = function (x, y){
-  ctx.lineWidth = 10;
+  ctx.lineWidth = 2;
   ctx.strokeStyle = "Black";
   ctx.fillStyle = "Gold";
 
-  circle(x,y,50,true);
-  circle(x,y,50,false);
-  circle(x-20,y-30,20,false);
-  circle(x+20,y-30,20,false);
+  circle(x,y,8,true);
+  circle(x,y,8,false);
+  circle(x-5,y-11,5,false);
+  circle(x+5,y-11,5,false);
 }
+
 var update = function(coordinate){
   var offset = Math.random()*4-2;
   coordinate += offset;
 
-  if(coordinate > 500){
-    coordinate = 500;
+  if(coordinate > 200){
+    coordinate = 200;
   }
 
   if(coordinate < 0){
